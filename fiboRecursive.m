0401ae0069fb5798d0e5f6afb9d30584b0778158
Chloe Lehnert <chloe.lehnert@gmail.com>
2021-04-15 15:27:54 -0500

    filled in and renamed file

---
+++ fiboRecursive.m
@@ -0,0 +1,9 @@
+function [f] = fiboRecursive(n)
+     global count;
+     if n == 1 || n == 2
+          f = 1;
+     else
+          count = count + 1;
+          f = fiboRecursive(n-1) + fiboRecursive(n-2);
+     end
+end
